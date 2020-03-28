be0fdbe8230395fd61e39a9744d573f5bc657f72
Dhruv <32142021+dhruv-kharwar@users.noreply.github.com>
2020-03-28 14:09:36 -0400

    GitHub demo

--- rethinking_setup.R
+++ rethinking_setup.R
@@ -1,14 +1,4 @@
 # SETUP
-# Removing any pre-installed versions of Stan:
-remove.packages("rstan")
-if (file.exists(".RData")) file.remove(".RData")
-
-# Installing RStan:
-install.packages("rstan", repos = "https://cloud.r-project.org/", dependencies = TRUE)
-
-# Checking C++ toolchain:
-pkgbuild::has_build_tools(debug = TRUE)
-
 # Installing rethinking:
 install.packages(c("coda", "mvtnorm", "devtools"))
 library(devtools)
